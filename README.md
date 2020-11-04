f397690953b5324c87b6d9f3a01af32f19650470
Shixiong Wang <62227601+Spratm-Asleaf@users.noreply.github.com>
2020-11-04 09:58:50 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# DRSE
-Online complementary materials of the paper titled  "Robust State Estimation for Linear Systems underDistributional Uncertainty"
+> @Author: WANG Shixiong (Email: <s.wang@u.nus.edu>; <wsx.gugo@gmail.com>)
+
+> @Affiliate: Department of Industrial SystemsEngineering and Management, National University of Singapore
+
+> @Date: 3 Nov, 2020
+
+> MATLAB Version: 2016A or later
+
+# DRSE: Distributionally Robust State Estimation
+
+Online supplementary materials of the paper titled 
+
+**Robust State Estimation for Linear Systems underDistributional Uncertainty**
+
+By Shixiong Wang, Zhongming Wu, and Andrew Lim
+
+From the Department of Industrial Systems Engineering and Management, National University of Singapore (S. Wang and A. Lim);
+and the School of Management Science and Engineering, Nanjing University of Information Science and Technology (Z. Wu).
+
+***Data***
+
+* The folder "[1] Data in Figures and Tables" contains the raw data to reproduce Figures 1, 2 and Tables I, II, III, IV, V.
+
+***Codes***
+
+* The folder "[2] Source Code - True Structure" contains the codes to generate the raw data in Figure 1 and Tables I, II, III, IV.
+
+* The folder "[3] Source Code - Fake Structure" contains the codes to generate the raw data in Figure 2 and Table V.
---
+++ [2] Source Code - True Structure/HINFTY.m
@@ -0,0 +1,57 @@
+%{
+Online supplementary materials of the paper titled 
+"Robust State Estimation for Linear Systems under Distributional Uncertainty"
+Authored By: Shixiong Wang, Zhongming Wu, and Andrew Lim
+
+@Author: Shixiong Wang
+@Date: 3 Nov 2020
+@Email: s.wang@u.nus.edu; wsx.gugo@gmail.com
+@Site: https://github.com/Spratm-Asleaf/DRSE
+
+Related Paper: [20]
+%}
+
+function [hat_X,P] = HINFTY(Y,F,G,H,Q,R,Pi_0,hat_x_0)
+[~,N] = size(Y);
+P = Pi_0;
+X = hat_x_0;
+[m,n] = size(H);
+
+hat_X = [];
+
+L = eye(n);
+
+gamma = eps;
+for i=1:N
+    while true
+        % Eq. (8)
+        if min(eig(P^-1 + H'*H - gamma^(-2)*(L'*L))) > 0
+            break;
+        else
+            gamma = gamma + 2;  % Can use any value except 1, otherwise the last entry (right bottom) of P will be zero
+        end
+    end
+    R_e = [eye(m), zeros(m,n); zeros(n,m), -(gamma^2)*eye(n)] + [H; L]*P*[H' L'];
+    P = F*P*F' + G*G' - F*P*[H' L']*R_e^-1*[H; L]*P*F';  % note that P' = P
+end
+
+% show the gamma
+disp(['{{ H-infinity :: gamma: ' num2str(gamma) '}}']);
+
+for i=1:N
+    X = F*X;
+    Z_ = H*X;
+
+    % Eq. (10)
+    R_e = [eye(m), zeros(m,n); zeros(n,m), -(gamma^2)*eye(n)] + [H; L]*P*[H' L'];
+    % Eq. (9)
+    P = F*P*F' + G*G' - F*P*[H' L']*R_e^-1*[H; L]*P*F';  % note that P' = P
+
+    % Eq. (12)
+    K = P*H'*(H*P*H' + eye(m))^-1;
+    
+    % Eq. (11)
+    X = X + K*(Y(i) - Z_);
+    
+    hat_X = [hat_X X];
+end
